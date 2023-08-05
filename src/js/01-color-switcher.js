c426ddc1f24e58fae88175c38d8d793deef5b4e6
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-08-05 23:26:17 +0300

    finished task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+let timerId = null;
+const SET_TIMER = 1000
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.stopBtn.disabled = true;
+refs.startBtn.addEventListener('click', onBtnStartChangeColor);
+refs.stopBtn.addEventListener('click', onBtnStopChangeColor);
+
+function onBtnStartChangeColor() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, SET_TIMER);
+}
+
+function onBtnStopChangeColor() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
